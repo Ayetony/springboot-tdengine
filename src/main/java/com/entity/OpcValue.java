a70761af50afba1be59c486fd8c8e24faa07b491
薛佳旺 <3086876977@qq.com>
2023-08-09 11:59:14 +0800

    添加类

---
+++ src/main/java/com/entity/OpcValue.java
@@ -0,0 +1,19 @@
+package com.entity;
+
+import java.util.Date;
+
+public class OpcValue {
+    /**
+     * 源时间
+     */
+    private Date sourceTime;
+    /**
+     * 服务器时间
+     */
+    private Date serverTime;
+
+    /**
+     * 点位值
+     */
+    private String value;
+}
