5ab6949d7b6f6d44c999232b70f9f1e6a1ff846e
苗永乐 <miao.ayetony@gmail.com>
2023-08-09 14:03:21 +0800

    Async thread pool set

--- src/main/java/com/DemoApp.java
+++ src/main/java/com/DemoApp.java
@@ -3,6 +3,7 @@ package com;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * @author CuiXi
@@ -12,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan("com.mapper")
+@EnableAsync
 public class DemoApp {
     public static void main(String[] args) {
         SpringApplication.run(DemoApp.class,args);
---
+++ src/main/java/com/iot/config/AsyncTaskExecutePool.java
@@ -0,0 +1,58 @@
+package com.iot.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * 异步任务线程池装配类
+ */
+@Slf4j
+@Configuration
+@EnableAsync
+public class AsyncTaskExecutePool implements AsyncConfigurer {
+
+    /** 注入配置类 */
+    private final AsyncTaskProperties config;
+
+    public AsyncTaskExecutePool(AsyncTaskProperties config) {
+        this.config = config;
+    }
+
+    @Bean("taskExecutor")
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        //核心线程池大小
+        executor.setCorePoolSize(config.getCorePoolSize());
+        //最大线程数
+        executor.setMaxPoolSize(config.getMaxPoolSize());
+        //队列容量
+        executor.setQueueCapacity(config.getQueueCapacity());
+        //活跃时间
+        executor.setKeepAliveSeconds(config.getKeepAliveSeconds());
+        //线程名字前缀
+        executor.setThreadNamePrefix("el-async-");
+        // setRejectedExecutionHandler：当pool已经达到max size的时候，如何处理新任务
+        // 线程池对拒绝任务的处理策略
+        // CallerRunsPolicy：由调用线程（提交任务的线程）处理该任务
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        // 初始化
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return (throwable, method, objects) -> {
+            log.error("====" + throwable.getMessage() + "====", throwable);
+            log.error("exception method:" + method.getName());
+        };
+    }
+}
