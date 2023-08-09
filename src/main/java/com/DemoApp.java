e86992a7047e9bf353eb887ef6df5eb0d71dd360
苗永乐 <miao.ayetony@gmail.com>
2023-08-09 13:04:18 +0800

    async annotation cancel

--- src/main/java/com/DemoApp.java
+++ src/main/java/com/DemoApp.java
@@ -3,7 +3,6 @@ package com;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * @author CuiXi
@@ -13,7 +12,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
  */
 @SpringBootApplication
 @MapperScan("com.mapper")
-@EnableAsync
 public class DemoApp {
     public static void main(String[] args) {
         SpringApplication.run(DemoApp.class,args);
